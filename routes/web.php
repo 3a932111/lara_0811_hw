b37d913f48f1dbd2d85ca10cd994d290c4b1bbaf
3a932111 <3a932111@gm.student.ncut.edu.tw>
2022-11-02 11:14:32 +0800

    新增controller及Router

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\NewsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/',[HomeController::class,'index']
+)->name("home.index");
+
+Route::get('/about',[AboutController::class,'index']
+)->name("about.index");
+
+Route::get('/news',[NewsController::class,'index']
+)->name("news.index");
